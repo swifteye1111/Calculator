8bf5b4fbbd14a8b45f50587e5044817f397c4693
swifteye1111 <hasak.josh@gmail.com>
2022-08-19 12:48:48 +0100

    Make math functions accessible through operate function

--- calc.js
+++ calc.js
@@ -1,15 +1,24 @@
-function add(a,b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
+}
+
+// Takes in an operation and two numbers and executes the operation on them.
+function operate(op, a, b) {
+    return (op === '+') ? add(a, b)
+    : (op === '-') ? subtract(a, b)
+    : (op === '*') ? multiply(a, b)
+    : (op === '/') ? divide(a, b)
+    : 'Invalid input';
 }
