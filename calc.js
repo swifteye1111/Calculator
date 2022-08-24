ca55173a54c70ade314703d95dc3226106f7148e
swifteye1111 <hasak.josh@gmail.com>
2022-08-24 17:16:03 +0100

    Add comments.

--- calc.js
+++ calc.js
@@ -31,9 +31,12 @@ let display = document.querySelector('.display');
 Array.from(buttons).forEach(button => {
     button.addEventListener('click', () => {
         if (button.textContent === '=') {
+            // Filter out the operators into an array.
             const ops = Array.from(displayValue).filter((item) => {
                 return !(item >= 0 || item <= 9 || item === '.');
             });
+
+            // Filter out the numbers into an array, including multiple digits.
             const nums = [];
             let num = '';
             let i = 0;
@@ -47,12 +50,14 @@ Array.from(buttons).forEach(button => {
                         i++;
                     }
             }
+            // Add the last digit to the array.
             nums.push(num);
 
             displayValue = '';
             display.textContent = runOperate(nums, ops);
 
         }   else if (button.textContent === 'AC') {
+            // Clear the display.
             displayValue = '';
             display.textContent = '';
         }   else {
@@ -62,12 +67,15 @@ Array.from(buttons).forEach(button => {
     })
 });
 
+// This function takes in an array of numbers and corresponding operators. It then evaluates
+//  the numbers and operators and returns the final answer.
 function runOperate(nums, ops) {
     let a = 0;
     for (let i = 0; i < nums.length; i++) {
         let j = i + 1;
         if (ops[j]) {
             a = operate(ops[i],nums[i],nums[j]);
+            // Put the result of the previous expression in the next number place to be evaluated.
             nums[j] = a;
         }   else {
                 // return answer rounded to the 4th decimal place
